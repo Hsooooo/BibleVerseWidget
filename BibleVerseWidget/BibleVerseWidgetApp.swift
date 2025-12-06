7e9c862d784d06f3cbedf68bf5db8d9ba0f77a7c
Hsooooo <rkrk6469@nate.com>
2025-12-06 10:03:30 +0900

    Initial Commit

---
+++ BibleVerseWidget/BibleVerseWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleVerseWidgetApp.swift
+//  BibleVerseWidget
+//
+//  Created by 이한수 on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BibleVerseWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
